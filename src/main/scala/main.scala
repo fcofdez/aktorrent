16c24a7d4b47b352ebcd96ebf1a61980cc17fd7e
Francisco Fernandez <francisco.fernandez.castano@gmail.com>
2016-05-18 01:21:35 +0200

    Adding new rules to parboiled grammar on bencoding.

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -1,23 +1,17 @@
 package aktorrent
 import java.nio.file.{Files, Paths}
+import scala.util.Success
 
 case class MetaInfo(anounce: List[String],
                     info: Option[BDict])
 
 object Test extends BencodeParser {
   def main(args: Array[String]) = {
-    val source = scala.io.Source.fromFile("/home/fran/github/aktorrent/l.torrent", "ISO-8859-15")
-    println(source.mkString)
+    // val source = scala.io.Source.fromFile("/home/fran/github/aktorrent/l.torrent", "ISO-8859-15")
+    // println(source.mkString)
 
     val byteArray = Files.readAllBytes(Paths.get("/home/fran/github/aktorrent/l.torrent"))
-    // new Calculator(new ByteReader(byteArray)).InputLine.run() // evaluates to `scala.util.Success(2)`
-    parse(new ByteReader(byteArray)).get match {
-      case BDict(info) =>
-        val a = "announce-list".toCharArray().map(_.toByte).toList
-        info.get(BString(a)) match {
-          case Some(get) =>
-            println(get)
-        }
-    }
+    val a = new Bencodingg(byteArray).InputLine.run()
+    println(a)
   }
 }
--- src/main/scala/partest.scala
+++ src/main/scala/partest.scala
@@ -1,21 +1,29 @@
 package aktorrent
 
 import org.parboiled2._
+import shapeless.HNil
+import shapeless.::
 
 class Bencodingg(val input: ParserInput) extends Parser {
+  private var fieldEnd: Int = _
+
   def InputLine = rule { root ~ EOI }
 
-  def root = rule { zeroOrMore(str | int | list) }
+  def root = rule { zeroOrMore(str | int | list | dict) }
 
-  def str = rule { Number ~ ':' ~> ((length: Int) => length.times(capture(CharPredicate.All)))}
+  def str: Rule1[String] = rule { Number ~ ':' ~> ((length: Long) => run(fieldEnd = cursor + length.toInt)) ~ capture(oneOrMore(test(cursor < fieldEnd) ~ CharPredicate.All))}
 
-  def int = rule { 'i' ~ integer ~ 'e' }
+  def int: Rule1[Long] = rule { 'i' ~ integer ~ 'e' }
+
+  def innerDict = rule { str ~ (str | int | list | dict ) ~> ((a: String, b: Any) => (a, b)) }
+
+  def dict: Rule1[Map[String, Any]] = rule { 'd' ~ oneOrMore(innerDict) ~ 'e' ~> ((tuples: Seq[Tuple2[String, Any]]) => tuples.toMap)}
 
   def integer = rule {('-' ~ Number ~> (_ * -1)) | Number }
 
-  def list: Rule1[Any] = rule {'l' ~ zeroOrMore(str | int | list) ~ 'e'}
+  def list: Rule1[Seq[Any]] = rule {'l' ~ zeroOrMore(str | int | list) ~ 'e'}
 
-  def Number = rule { capture(Digits) ~> (_.toInt) }
+  def Number = rule { capture(Digits) ~> (_.toLong) }
 
   def Digits = rule { oneOrMore(CharPredicate.Digit) }
 }
