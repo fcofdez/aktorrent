9f4bb14b77da0cbe460a44745822890555f111d3
Francisco Fernandez <francisco.fernandez.castano@gmail.com>
2016-11-09 20:41:36 +0100

    Adding signature to Test object.

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -3,7 +3,7 @@ package aktorrent
 import java.nio.file.Paths
 
 object Test {
-  def main(args: Array[String]) = {
+  def main(args: Array[String]): Unit = {
     MetaInfoReader.read(Paths.get("/home/fran/github/aktorrent/kat.torrent"))
   }
 }
